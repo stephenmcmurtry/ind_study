d0f98c52f9fc0443a34bd0e84f13a83ea2361801
stephenmcmurtry <stephen.mcmurtry@outlook.com>
2016-05-18 14:46:30 -0400

    Finishing touches

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 var feed = require('./routes/feed');
 var story = require('./routes/story');
+var about = require('./routes/about');
 
 // Start web server and socket.io server
 var app = express();
@@ -36,6 +37,7 @@ app.use('/', routes);
 app.use('/users', users);
 app.use('/feed', feed);
 app.use('/stories', story);
+app.use('/about', about);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
